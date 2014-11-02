a782a7a61de3ac0f9b26c33efe10337a058c1215
Alex Pop <palexandru@gmail.com>
2014-11-02 20:20:58 +0000

    change initial output code

--- getting-started/underscore.map.reduce-with-jasmine/index.js
+++ getting-started/underscore.map.reduce-with-jasmine/index.js
@@ -1,10 +1,10 @@
 $(document).ready(function() {
   $("#output").html("<br />Award age for people:");
-  _.each(awardAgeCalculator.getPeopleWithAwardAge(), function(person) {
+  _.each(awardAgeCalculator.getPeopleWithAwardAge(awardAgeCalculator.getPeople), function(person) {
     var existingContent = $("#output").html();
     var newContent = " - " + person.name + " was " + person.awardAge + " years old";
     $("#output").html(existingContent + "<br />" + newContent);
   });
 
-  $("#output").html($("#output").html() + "<br /><br />" + "Average award age is " + Math.floor(awardAgeCalculator.getAverageAwardAge()) + " years old.");
+  $("#output").html($("#output").html() + "<br /><br />" + "Average award age is " + Math.floor(awardAgeCalculator.getAverageAwardAge(awardAgeCalculator.getPeople)) + " years old.");
 });
