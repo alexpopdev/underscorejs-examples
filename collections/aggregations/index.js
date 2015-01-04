0c146d6d5d409338940d83f515b254a67a0338f2
Alex Pop <palexandru@gmail.com>
2015-01-04 17:04:01 +0000

    fixed decimals for browser output

--- collections/aggregations/index.js
+++ collections/aggregations/index.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 
-  var displayContent = "Average rental price for all bicycles: " + bicycleAggregator.getAverageRentalPrice();
-  displayContent += "<br/><br/>Average rental price for urban bicycles: " + bicycleAggregator.getAverageRentalPrice("Urban Bike");
+  var displayContent = "Average rental price for all bicycles: " + bicycleAggregator.getAverageRentalPrice().toFixed(2);
+  displayContent += "<br/><br/>Average rental price for urban bicycles: " + bicycleAggregator.getAverageRentalPrice("Urban Bike").toFixed(2);
   $("#output").html(
     "<h2>Aggregations over bicycles:</h2>" +
     displayContent);
