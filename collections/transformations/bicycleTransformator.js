83cac2af3e8d1e8f5179f3353554439897564f5c
Alex Pop <palexandru@gmail.com>
2015-01-08 23:31:21 +0000

    added first transformation example

---
+++ collections/transformations/bicycleTransformator.js
@@ -0,0 +1,113 @@
+var bicycleTransformator = (function() {
+  "use strict";
+
+  var getBicycles = function() {
+    return [{
+      id: 1,
+      name: "A fast bike",
+      type: "Road Bike",
+      quantity: 10,
+      rentPrice: 20,
+      dateAdded: new Date(2015, 1, 2)
+    }, {
+      id: 2,
+      name: "An even faster bike",
+      type: "Road Bike",
+      quantity: 4,
+      rentPrice: 25,
+      dateAdded: new Date(2015, 2, 25)
+    }, {
+      id: 3,
+      name: "A springy bike",
+      type: "Mountain Bike",
+      quantity: 20,
+      rentPrice: 18,
+      dateAdded: new Date(2014, 10, 1)
+    }, {
+      id: 4,
+      name: "A springier bike",
+      type: "Mountain Bike",
+      quantity: 10,
+      rentPrice: 22,
+      dateAdded: new Date(2014, 4, 1)
+    }, {
+      id: 5,
+      name: "An all-terain bike",
+      type: "Mountain Bike",
+      quantity: 5,
+      rentPrice: 27,
+      dateAdded: new Date(2014, 8, 14)
+    }, {
+      id: 6,
+      name: "A classy bike",
+      type: "Urban Bike",
+      quantity: 30,
+      rentPrice: 15,
+      dateAdded: new Date(2014, 6, 27)
+    }, {
+      id: 7,
+      name: "A modern bike",
+      type: "Urban Bike",
+      quantity: 20,
+      rentPrice: 17,
+      dateAdded: new Date(2015, 1, 19)
+    }, {
+      id: 8,
+      name: "A commuter bike",
+      type: "Urban Bike",
+      quantity: 12,
+      rentPrice: 14,
+      dateAdded: new Date(2014, 8, 2)
+    }, {
+      id: 9,
+      name: "A blue bike",
+      type: "Children Bike",
+      quantity: 25,
+      rentPrice: 10,
+      dateAdded: new Date(2014, 10, 20)
+    }, {
+      id: 10,
+      name: "A pink bike",
+      type: "Children Bike",
+      quantity: 25,
+      rentPrice: 10,
+      dateAdded: new Date(2015, 2, 5)
+    }, {
+      id: 11,
+      name: "A noisy bike",
+      type: "Children Bike",
+      quantity: 3,
+      rentPrice: 12,
+      dateAdded: new Date(2014, 8, 23)
+    }, {
+      id: 12,
+      name: "A clown bike",
+      type: "Children Bike",
+      quantity: 2,
+      rentPrice: 12,
+      dateAdded: new Date(2014, 11, 1)
+    }];
+  };
+
+  var filterBicyclesByType = function(bicycles, type) {
+    if (!type) {
+      return bicycles;
+    }
+
+    return _.where(bicycles, {
+      type: type
+    });
+  };
+
+  return {
+    getBicyclesSortedByRentPrice: function(type) {
+      var filteredBicycles = filterBicyclesByType(getBicycles(), type);
+
+      var sortedBicycles = _.sortBy(filteredBicycles, 'rentPrice');
+      return _.map(sortedBicycles, function(bicycle) {
+        return bicycle.name + " - " + bicycle.type + " : " + bicycle.rentPrice;
+      });
+    }
+  };
+
+}());
---
+++ collections/transformations/index.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+
+  var displayContent = "All bicycles sorted by rent price: <ul><li>" + bicycleTransformator.getBicyclesInfoSortedByRentPrice().join('</li><li>') + "</li></ul>";
+
+  // displayContent += "<br/><br/>Average rental price for urban bicycles: " + bicycleAggregator.getAverageRentalPrice("Urban Bike").toFixed(2);
+  //
+  // var bicycle = bicycleAggregator.getMostRecentlyAddedBicycle();
+  //
+  // displayContent += "<br/><br/>Most recently added bicycle: '" + bicycle.name + "' on: " + bicycle.dateAdded.toISOString();
+  //
+  // bicycle = bicycleAggregator.getMostRecentlyAddedBicycle("Urban Bike");
+  //
+  // displayContent += "<br/><br/>Most recently added urban bicycle: '" + bicycle.name + "' on: " + bicycle.dateAdded.toISOString();
+  //
+  // bicycle = bicycleAggregator.getLowestPricedBicycle("Urban Bike");
+  //
+  // displayContent += "<br/><br/>Lowest priced urban bicycle: '" + bicycle.name + "' with rental price: " + bicycle.rentPrice;
+
+  $("#output").html(
+    "<h2>Transformations with bicycles:</h2>" +
+    displayContent);
+});
