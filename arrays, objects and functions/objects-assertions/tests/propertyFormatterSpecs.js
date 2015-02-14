0d2abf9bea25e7c203d663f620ee4bec14013821
Alex Pop <palexandru@gmail.com>
2015-02-14 13:08:13 +0000

    added base code for objects-assertions examples

---
+++ arrays, objects and functions/objects-assertions/tests/propertyFormatterSpecs.js
@@ -0,0 +1,103 @@
+describe("Given propertyFormatter", function() {
+
+  describe("when calling extractPropertiesForDisplay(object)", function() {
+    var propertiesForDisplay;
+
+    beforeEach(function() {
+      var source = {
+        id: 2,
+        name: "Blue lamp",
+        description: null,
+        ui: undefined,
+        price: 10,
+        purchaseDate: new Date(2014, 10, 1),
+        isInUse: true,
+      };
+
+      propertiesForDisplay = propertyFormatter.extractPropertiesForDisplay(source);
+    });
+
+    it("then the returned property count should be correct", function() {
+      expect(propertiesForDisplay.length).toEqual(7);
+    });
+
+    it("then the 'price' property should be displayed", function() {
+      expect(propertiesForDisplay[4]).toMatch("price.+10");
+    });
+
+    it("then the 'description' property should not be displayed", function() {
+      expect(propertiesForDisplay[2]).toMatch("cannot be displayed");
+    });
+  });
+
+  describe("when calling extractPropertiesForDisplay({}, true) (for an empty object)", function() {
+    var propertiesForDisplay;
+
+    beforeEach(function() {
+      propertiesForDisplay = propertyFormatter.extractPropertiesForDisplay({}, true);
+    });
+
+    it("then the returned property count should be correct", function() {
+      expect(propertiesForDisplay.length).toEqual(0);
+    });
+  });
+
+  describe("when calling extractDataPropertiesForDisplay(object)", function() {
+    var propertiesForDisplay;
+
+    beforeEach(function() {
+      var source = {
+        id: 2,
+        name: "Blue lamp",
+        description: null,
+        ui: undefined,
+        price: 10,
+        purchaseDate: new Date(2014, 10, 1),
+        isInUse: true,
+        getProfit: function() {
+          return this.price * 0.25;
+        }
+      };
+
+      propertiesForDisplay = propertyFormatter.extractDataPropertiesForDisplay(source);
+    });
+
+    it("then the returned property count should be correct", function() {
+      expect(propertiesForDisplay.length).toEqual(7);
+    });
+
+    it("then the 'price' property should be displayed", function() {
+      expect(propertiesForDisplay[4]).toMatch("price.+10");
+    });
+
+    it("then the 'description' property should not be displayed", function() {
+      expect(propertiesForDisplay[2]).toMatch("cannot be displayed");
+    });
+  });
+
+  describe("when calling extractAllPropertiesForDisplay(object)", function() {
+    var allPropertiesForDisplay;
+
+    beforeEach(function() {
+      var source = {
+        id: 2,
+        name: "Blue lamp",
+        description: null,
+        ui: undefined,
+        price: 10,
+        purchaseDate: new Date(2014, 10, 1),
+        isInUse: true,
+      };
+
+      allPropertiesForDisplay = propertyFormatter.extractAllPropertiesForDisplay(source);
+    });
+
+    it("then the returned string has expected length", function() {
+      expect(allPropertiesForDisplay.length).toBeGreaterThan(0);
+    });
+
+    it("then the 'price' property should be displayed", function() {
+      expect(allPropertiesForDisplay).toMatch("<br/>Property: price of type: number has value: 10<br/>");
+    });
+  });
+});
