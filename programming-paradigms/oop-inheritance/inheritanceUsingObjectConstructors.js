209c526dafe6646c051119dac34d800ad0e4ac26
Alex Pop <palexandru@gmail.com>
2015-03-01 00:42:44 +0000

    added up examples

---
+++ programming-paradigms/oop-inheritance/inheritanceUsingObjectConstructors.js
@@ -0,0 +1,56 @@
+(function() {
+  "use strict";
+  var exampleTitle = "Inheritance using object constructors - ";
+
+  var originalClient = {
+    "id": 1,
+    "name": "Baxter Brooks",
+    "gender": "male",
+    "type": "client",
+    "email": "baxterbrooks@dymi.com",
+    "registered": "2014-03-15T10:52:05 -00:00",
+    "isActive": false
+  };
+
+  function Contact(id, name, gender, email) {
+    this.id = id;
+    this.name = name;
+    this.gender = gender;
+    this.type = "contact";
+    this.email = email;
+  }
+  var contact = new Contact(
+    1,
+    "Baxter Brooks",
+    "male",
+    "baxterbrooks@dymi.com"
+  );
+
+  console.log(exampleTitle + "Assert that [[Prototype]] for 'contact' instance is the object constructor prototype: " + (contact.__proto__ === Contact.prototype));
+
+  function Client(id, name, gender, email, registered, isActive) {
+    Contact.call(this, id, name, gender, email);
+    this.type = 'client';
+    this.registered = registered;
+    this.isActive = isActive;
+  }
+
+  Client.prototype = Object.create(Contact.prototype);
+  Client.prototype.constructor = Client;
+
+  var client = new Client(
+    1,
+    "Baxter Brooks",
+    "male",
+    "baxterbrooks@dymi.com",
+    "2014-03-15T10:52:05 -00:00",
+    false
+  );
+
+  console.log(exampleTitle + "Assert that [[Prototype]] for 'client' inherits from the [[Prototype]] for 'contact': " + (client.__proto__.__proto__ === contact.__proto__));
+
+  Contact.prototype.getContactIdAndName = function() {
+    return this.name + " (" + this.id + ")";
+  };
+  console.log(exampleTitle + "Assert that all objects inheriting from 'Contact' or that are 'Contact' have the same value for getContactIdAndName(): " + (contact.getContactIdAndName() === client.getContactIdAndName()));
+}());
---
+++ programming-paradigms/oop-inheritance/inheritanceUsingObjectLiterals.js
@@ -0,0 +1,54 @@
+(function() {
+  "use strict";
+  var exampleTitle = "Inheritance using object literals - ";
+
+  var originalClient = {
+    "id": 1,
+    "name": "Baxter Brooks",
+    "gender": "male",
+    "type": "client",
+    "email": "baxterbrooks@dymi.com",
+    "registered": "2014-03-15T10:52:05 -00:00",
+    "isActive": false,
+  };
+
+  var contact = {
+    "id": 1,
+    "name": "Baxter Brooks",
+    "gender": "male",
+    "type": "contact",
+    "email": "baxterbrooks@dymi.com",
+  };
+
+  var assertObjectLiteralPrototype = originalClient.__proto__ === contact.__proto__ && contact.__proto__ === {}.__proto__;
+  console.log(exampleTitle + "Assert that the default [[Prototype]] property of any object literal is the same object instance: " + assertObjectLiteralPrototype);
+
+  var client1 = {
+    __proto__: contact,
+    "type": "client",
+    "registered": "2014-03-15T10:52:05 -00:00",
+    "isActive": false,
+  };
+
+  var assertClient1Prototype = client1.__proto__ === Object.getPrototypeOf(client1) && client1.__proto__ === contact;
+  console.log(exampleTitle + "Assert that [[Prototype]] for object 'client1' is 'contact': " + assertClient1Prototype);
+
+  var client2 = Object.create(contact);
+  _.extend(client2, {
+    "type": "client",
+    "registered": "2014-03-15T10:52:05 -00:00",
+    "preferredBike": "A clown bike",
+    "isActive": false,
+  });
+
+  var assertClient2Prototype = client2.__proto__ === Object.getPrototypeOf(client2) && client2.__proto__ === contact;
+  console.log(exampleTitle + "Assert that [[Prototype]] for object 'client2' is 'contact': " + assertClient2Prototype);
+
+  client1.__proto__.emailSubscriber = true;
+  console.log(exampleTitle + "Assert that all objects inheriting from 'contact' and 'contact' itself have emailSubscriber set to true: " + (contact.emailSubscriber && client1.emailSubscriber && client2.emailSubscriber));
+
+  client1.__proto__.getContactIdAndName = function() {
+    return this.name + " (" + this.id + ")";
+  };
+  console.log(exampleTitle + "Assert that all objects inheriting from 'contact' and 'contact' itself have the same value for getContactIdAndName(): " + (contact.getContactIdAndName() === client1.getContactIdAndName() && contact.getContactIdAndName() === client2.getContactIdAndName()));
+}());
