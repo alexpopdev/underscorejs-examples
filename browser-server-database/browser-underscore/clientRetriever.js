e0682732f777e97b71b746dc6416cba7511d1156
Alex Pop <palexandru@gmail.com>
2015-03-18 20:39:48 +0000

    added slightly modified copy of fp-underscore

---
+++ browser-server-database/browser-underscore/clientRetriever.js
@@ -0,0 +1,42 @@
+var clientRetriever = (function() {
+  "use strict";
+
+  var getContacts = function() {
+    var clientObjects = dataProvider.getClients();
+    return _.map(clientObjects, function(clientObject) {
+      if (!clientObject.isActive) {
+        return new Contact(
+          clientObject.id,
+          clientObject.name,
+          clientObject.gender,
+          clientObject.company,
+          clientObject.email,
+          clientObject.phone,
+          clientObject.address);
+      }
+      return new Client(
+        clientObject.id,
+        clientObject.name,
+        clientObject.gender,
+        clientObject.company,
+        clientObject.email,
+        clientObject.phone,
+        clientObject.address,
+        new Date(clientObject.registered),
+        clientObject.preferredBike,
+        clientObject.bikePoints,
+        clientObject.notes
+      );
+    });
+  };
+
+  return {
+    getContacts: getContacts,
+    getClients: function() {
+      var contacts = getContacts();
+      return _.filter(contacts, function(contact) {
+        return contact.constructor === Client;
+      });
+    }
+  };
+}());
---
+++ browser-server-database/browser-underscore/index.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+
+  var contacts = clientRetriever.getContacts();
+  var displayContent = "There are " + contacts.length + " contacts.<br />";
+
+  displayContent += "First 4 contacts with name, id and type: <ul><li>" +
+    _.map(_.first(contacts, 4), function(contact) {
+      return transformations.getContactNameIdAndType(contact);
+    }).join('</li><li>') +
+    "</li></ul>";
+
+  var clients = clientRetriever.getClientsUsingConstructorInfo();
+  displayContent += "There are " + clients.length + " active clients.<br />";
+  $("#output").html(
+    "<h2>FP with Underscore examples:</h2>" +
+    displayContent);
+});
