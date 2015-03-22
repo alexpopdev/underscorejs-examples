b1e2bf49172c017dc6d04c1fb511b4de0bfadd17
Alex Pop <palexandru@gmail.com>
2015-03-22 07:50:23 +0000

    renamed example

--- browser-server-database/browser-underscore-templates/clientRetriever.js
+++ browser-server-database/browser-underscore-templates/clientRetriever.js
@@ -37,6 +37,12 @@ var clientRetriever = (function() {
       return _.filter(contacts, function(contact) {
         return contact.constructor === Client;
       });
+    },
+    getOldestClients: function(count) {
+      return _.first(_.sortBy(this.getClients(), 'registered'), count);
+    },
+    getBestClients: function(count) {
+      return _.first(_.sortBy(this.getClients(), 'bikePoints'), count);
     }
   };
 }());
