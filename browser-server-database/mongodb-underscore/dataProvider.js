28773a6efdd37cc32b55957c94a060cbed51299b
Alex Pop <palexandru@gmail.com>
2015-04-18 07:35:53 +0100

    minor fix

--- browser-server-database/mongodb-underscore/dataProvider.js
+++ browser-server-database/mongodb-underscore/dataProvider.js
@@ -1,4 +1,4 @@
-var MongoClient = require('mongodb').MongoClient
+var MongoClient = require('mongodb').MongoClient;
 var dbUrl = 'mongodb://localhost:27017/underscorejs-examples';
 
 var getBicycles = function(callback) {
