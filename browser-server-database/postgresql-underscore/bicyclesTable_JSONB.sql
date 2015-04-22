afaffdf80723020290a25c1863d7370191fa1be1
Alex Pop <palexandru@gmail.com>
2015-04-22 18:36:44 +0100

    changed log messages

--- browser-server-database/postgresql-underscore/bicyclesTable_JSONB.sql
+++ browser-server-database/postgresql-underscore/bicyclesTable_JSONB.sql
@@ -4,19 +4,29 @@ CREATE TABLE bicycles(
 );
 INSERT INTO bicycles (id, body)
 VALUES
-(1, '{
-        "id": 1,
-        "name": "A fast bike",
-        "type": "Road Bike",
-        "quantity": 10,
-        "rentPrice": 20,
-        "dateAdded": "2015-02-02T00:00:00.000Z"
-      }');
+(1, '{ "id": 1,
+       "name": "A fast bike",
+       "type": "Road Bike",
+       "quantity": 10,
+       "rentPrice": 20,
+      "dateAdded": "2015-02-02T00:00:00.000Z" }'),
+(2, '{ "id": 2,
+       "name": "An even faster bike",
+       "type": "Road Bike",
+       "quantity": 4,
+       "rentPrice": 25,
+       "dateAdded": "2015-03-25T00:00:00.000Z" }'),
+(3, '{ "id": 3,
+       "name": "A springy bike",
+       "type": "Mountain Bike",
+       "quantity": 20,
+       "rentPrice": 18,
+       "dateAdded": "2014-11-01T00:00:00.000Z" }');
 
-SELECT 'Get bicycles:';
+SELECT 'Get bicycles as documents:' AS "Log message";
 SELECT * FROM bicycles;
 
-SELECT 'Get bicycles as rows converted from jsonb with jsonb_to_record(body):';
+SELECT 'Get bicycles as rows converted from jsonb with jsonb_to_record(body):' AS "Log message";
 SELECT converted.id, name, type, quantity, "rentPrice","dateAdded" FROM bicycles, jsonb_to_record(body) AS
   converted (
     id int,
@@ -26,11 +36,11 @@ SELECT converted.id, name, type, quantity, "rentPrice","dateAdded" FROM bicycles
     "rentPrice" numeric,
     "dateAdded" timestamp);
 
-SELECT 'Get bicycles as rows converted from jsonb with field selectors:';
+SELECT 'Get bicycles as rows converted from jsonb using field selectors:' AS "Log message";
 SELECT  body->'id' AS id,
         body->'name' AS name,
         body->'type' AS type,
-        body->'quantity' AS quantity,
+        (body->>'quantity')::smallint AS quantity,
         body->'rentPrice' AS rent_price,
         (body->>'dateAdded')::timestamp AS date_added
 FROM bicycles;
