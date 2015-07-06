d4fe9b41b5dc0868aa4469cb29cdd191a4c5caca
Alex Pop <palexandru@gmail.com>
2015-07-06 22:52:54 +0100

    prepared sample skeleton

--- advanced-topics/underscore-contrib/contribSamples.js
+++ advanced-topics/underscore-contrib/contribSamples.js
@@ -1,38 +1,8 @@
-var clientRetriever = (function() {
+var contribSamples = (function() {
   "use strict";
 
-  var getContacts = function() {
-    var clientObjects = dataProvider.getClients();
-    return _.map(clientObjects, function(clientObject) {
-      if (!clientObject.isActive) {
-        return new Contact(
-          clientObject.id,
-          clientObject.name,
-          clientObject.gender,
-          clientObject.company,
-          clientObject.email,
-          clientObject.phone,
-          clientObject.address);
-      }
-      return new Client(
-        clientObject.id,
-        clientObject.name,
-        clientObject.gender,
-        clientObject.company,
-        clientObject.email,
-        clientObject.phone,
-        clientObject.address,
-        new Date(clientObject.registered),
-        clientObject.preferredBike,
-        clientObject.bikePoints,
-        clientObject.notes
-      );
-    });
-  };
-
   return {
-    getContacts: getContacts,
-    getClients: function() {
+    getArrayFromArguments: function() {
       var contacts = getContacts();
       return _.filter(contacts, function(contact) {
         return contact.constructor === Client;
