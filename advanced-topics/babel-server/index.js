efba2d3719124e3093d9a8aceb9a857f5d538b75
Alex Pop <palexandru@gmail.com>
2015-08-04 13:17:41 +0100

    restricted babel transpiration to the es6 folder

--- advanced-topics/babel-server/index.js
+++ advanced-topics/babel-server/index.js
@@ -1,4 +1,9 @@
-require("babel/register");
+require("babel/register")({
+  // Optional only regex - if any filenames **don't** match this regex then they
+  // aren't compiled
+  only: /es6/
+});
+
 var getExampleOutput = require("./es6/example");
 
 console.log(getExampleOutput());
