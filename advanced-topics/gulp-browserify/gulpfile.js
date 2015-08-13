1940f408e2846a4306f733b261ca7291abd0fc53
Alex Pop <palexandru@gmail.com>
2015-08-13 19:05:37 +0100

    reseeded the example from gulp-automation

--- advanced-topics/gulp-browserify/gulpfile.js
+++ advanced-topics/gulp-browserify/gulpfile.js
@@ -1,15 +1,32 @@
 var gulp = require('gulp');
 var concat = require('gulp-concat');
+var open = require('gulp-open');
 
 var appScripts = 'app/**/*.js';
-gulp.task('build', function() {
+gulp.task('build-app', function() {
   gulp.src(appScripts)
     .pipe(concat('app.js'))
     .pipe(gulp.dest('./public/'));
 });
 
-gulp.task('build-for-test', function() {
+gulp.task('build-tests', function() {
   gulp.src([appScripts, '!app/index.js'])
     .pipe(concat('app.js'))
     .pipe(gulp.dest('./tests/public/'));
+});
+
+gulp.task('open-app', ['build-app'], function() {
+  gulp.src('./index.html')
+    .pipe(open());
+});
+
+gulp.task('open-tests', ['build-tests'], function() {
+  gulp.src('./SpecRunner.html')
+    .pipe(open({
+      app: 'firefox'
+    }));
+});
+
+gulp.task('default', ['open-app', 'open-tests'], function() {
+  // using the default task to trigger all the other tasks for this project
 });
