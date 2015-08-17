31aba345296b49a24c0700c8092286faee00221d
Alex Pop <palexandru@gmail.com>
2015-08-17 20:17:16 +0100

    converted specs to es6 and executed them with jasmine-es6

--- advanced-topics/gulp-browserify-es6/spec/ClientSpec.js
+++ advanced-topics/gulp-browserify-es6/spec/ClientSpec.js
@@ -1,11 +1,12 @@
-var Client = require("../app/client.js");
-describe("Given Client class", function() {
+/*jshint esnext: true */
+import Client from "../app/client.es6";
+describe("Given Client class", () => {
 
-  describe("when calling new Client() with too few arguments", function() {
+  describe("when calling new Client() with too few arguments", () => {
     var createClient = function() {
       new Client(1, " A contact name");
     };
-    it("then throws the correct exception", function() {
+    it("then throws the correct exception", () => {
       expect(createClient).toThrow({
         name: 'ArgumentsException',
         message: 'The arguments length is incorrect.'
@@ -13,12 +14,12 @@ describe("Given Client class", function() {
     });
   });
 
-  describe("when calling new Client() with an argument of the wrong type", function() {
+  describe("when calling new Client() with an argument of the wrong type", () => {
     var createClient = function() {
       new Client(1, " A contact name", "Female", "Company2", "An email", "Phone1", "An address",
         "Date2014", "A bike", 200, "Some notes ");
     };
-    it("then throws the correct exception", function() {
+    it("then throws the correct exception", () => {
       expect(createClient).toThrow({
         name: "ArgumentsException",
         message: "One of the arguments does not have the expected type."
--- advanced-topics/gulp-browserify-es6/spec/clientRetrieverSpec.js
+++ advanced-topics/gulp-browserify-es6/spec/clientRetrieverSpec.js
@@ -1,19 +1,20 @@
-var clientRetriever = require("../app/clientRetriever.js");
-describe("Given clientRetriever", function() {
+/*jshint esnext: true */
+import * as clientRetriever from "../app/clientRetriever.es6";
+describe("Given clientRetriever", () => {
 
-  describe("when calling getContacts()", function() {
+  describe("when calling getContacts()", () => {
     var contacts = clientRetriever.getContacts();
 
-    it("then it returns an array of the correct length", function() {
+    it("then it returns an array of the correct length", () => {
       expect(contacts).toBeTruthy();
       expect(contacts.length).toEqual(250);
     });
   });
 
-  describe("when calling getClients()", function() {
+  describe("when calling getClients()", () => {
     var clients = clientRetriever.getClients();
 
-    it("then it returns an array of the correct length", function() {
+    it("then it returns an array of the correct length", () => {
       expect(clients).toBeTruthy();
       expect(clients.length).toEqual(121);
     });
