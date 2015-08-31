3257daebdb4ea548c30f415ed4540268682c968b
Alex Pop <palexandru@gmail.com>
2015-08-31 10:24:28 +0100

    improved the arrays example

--- arrays-objects-functions/arrays/clientRetriever.js
+++ arrays-objects-functions/arrays/clientRetriever.js
@@ -4,37 +4,57 @@ var clientRetriever = (function() {
   var clients = dataProvider.getClients();
   return {
     getNewestClients: function(count) {
-      return _.last(_.sortBy(clients, 'registered'), count);
+      return _.last(_.sortBy(clients, function(client) {
+        return new Date(client.registered);
+      }), count);
     },
     getOldestClients: function(count) {
-      return _.first(_.sortBy(clients, 'registered'), count);
+      return _.first(_.sortBy(clients, function(client) {
+        return new Date(client.registered);
+      }), count);
     },
     getOlderClients: function(skipLastCount) {
-      return _.initial(_.sortBy(clients, 'registered'), skipLastCount);
+      return _.initial(_.sortBy(clients, function(client) {
+        return new Date(client.registered);
+      }), skipLastCount);
     },
     getNewerClients: function(skipFirstCount) {
-      return _.rest(_.sortBy(clients, 'registered'), skipFirstCount);
+      return _.rest(_.sortBy(clients, function(client) {
+        return new Date(client.registered);
+      }), skipFirstCount);
     },
     getOldestOrBestClients: function(count) {
-      var oldestClients = _.first(_.sortBy(clients, 'registered'), count);
+      var oldestClients = _.first(_.sortBy(clients, function(client) {
+          return new Date(client.registered);
+        }),
+        count);
       var bestClients = _.last(_.sortBy(clients, 'bikePoints'), count);
       var oldestOrBestClients = _.union(oldestClients, bestClients);
       return oldestOrBestClients;
     },
     getOldestOrBestClientsWithuniq: function(count) {
-      var oldestClients = _.first(_.sortBy(clients, 'registered'), count);
+      var oldestClients = _.first(_.sortBy(clients, function(client) {
+          return new Date(client.registered);
+        }),
+        count);
       var bestClients = _.last(_.sortBy(clients, 'bikePoints'), count);
       var oldestOrBestClientsWithDuplicates = oldestClients.concat(bestClients);
       return _.uniq(oldestOrBestClientsWithDuplicates);
     },
     getOldestAndBestClients: function(count) {
-      var oldestClients = _.first(_.sortBy(clients, 'registered'), count);
+      var oldestClients = _.first(_.sortBy(clients, function(client) {
+          return new Date(client.registered);
+        }),
+        count);
       var bestClients = _.last(_.sortBy(clients, 'bikePoints'), count);
       var oldestAndBestClients = _.intersection(oldestClients, bestClients);
       return oldestAndBestClients;
     },
     getOldestOrBestClientsThatAreNotBoth: function(count) {
-      var oldestClients = _.first(_.sortBy(clients, 'registered'), count);
+      var oldestClients = _.first(_.sortBy(clients, function(client) {
+          return new Date(client.registered);
+        }),
+        count);
       var bestClients = _.last(_.sortBy(clients, 'bikePoints'), count);
       var oldestOrBestClients = _.union(oldestClients, bestClients);
       var oldestAndBestClients = _.intersection(oldestClients, bestClients);
