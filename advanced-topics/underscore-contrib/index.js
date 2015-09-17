c728d6aa03390bf9a5f406ddaf048d23be00a173
Alex Pop <palexandru@gmail.com>
2015-09-18 00:09:36 +0100

    updated packages

--- advanced-topics/underscore-contrib/index.js
+++ advanced-topics/underscore-contrib/index.js
@@ -1,16 +1,7 @@
 $(document).ready(function() {
 
-  var result = contribSamples.concatenateArrays(1, [5, , 64], null, [5,6,7], [1, , 2, null, 3], 99);
+  var result = contribSamples.concatenateArrays(1, [5, , 64], null, [5, 6, 7], [1, , 2, null, 3], 99);
   var displayContent = "Function concatenateArrays() returns " + result.length + " elements.<br />";
-  //
-  // displayContent += "First 4 contacts with name, id and type: <ul><li>" +
-  //   _.map(_.first(contacts, 4), function(contact) {
-  //     return contact.getContactNameIdAndType();
-  //   }).join('</li><li>') +
-  //   "</li></ul>";
-  //
-  // var clients = clientRetriever.getClientsUsingConstructorInfo();
-  // displayContent += "There are " + clients.length + " active clients.<br />";
   $("#output").html(
     "<h2>Underscore-contrib examples:</h2>" +
     displayContent);
